65759474520c3e9acb2cd2dcd4efa58f2abcbc6a
rumroses12 <15791887+gdaher@users.noreply.github.com>
2023-01-09 21:11:19 -0500

    table created

--- src/components/SimplePanel.tsx
+++ src/components/SimplePanel.tsx
@@ -1,61 +1,67 @@
-import React from 'react';
-import { PanelProps } from '@grafana/data';
-import { SimpleOptions } from 'types';
-import { css, cx } from '@emotion/css';
-import { useStyles2, useTheme2 } from '@grafana/ui';
+import { useEffect, useState } from "react";
+import { LoadingPlaceholder, Table, useTheme2 } from "@grafana/ui";
+import {
+  toDataFrame,
+  applyFieldOverrides,
+  DataFrame,
+  FieldType,
+  MutableDataFrame,
+  ThresholdsMode
+} from "@grafana/data";
 
-interface Props extends PanelProps<SimpleOptions> {}
-
-const getStyles = () => {
-  return {
-    wrapper: css`
-      font-family: Open Sans;
-      position: relative;
-    `,
-    svg: css`
-      position: absolute;
-      top: 0;
-      left: 0;
-    `,
-    textBox: css`
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      padding: 10px;
-    `,
-  };
-};
-
-export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+export default function SimplePanel(props) {
+  const [data, setData] = useState(null);
   const theme = useTheme2();
-  const styles = useStyles2(getStyles);
-  return (
-    <div
-      className={cx(
-        styles.wrapper,
-        css`
-          width: ${width}px;
-          height: ${height}px;
-        `
-      )}
-    >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: theme.colors.primary.main }} r={100} />
-        </g>
-      </svg>
 
-      <div className={styles.textBox}>
-        {options.showSeriesCount && <div>Number of series: {data.series.length}</div>}
-        <div>Text option value: {options.text}</div>
-      </div>
-    </div>
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+
+        let dataFrame = toDataFrame(data.products);
+
+        const configOverrides = {
+          rating: {
+            custom: {
+              displayMode: 'gradient-gauge',
+            },
+            min: 0,
+            max: 5
+          },
+          thumbnail: {
+            custom: {
+              displayMode: 'image'
+            }
+          }
+        }
+
+        for (const field of dataFrame.fields) {
+          field.config = { ...field.config, ...configOverrides[field.name] };
+        }
+        setData(applyFieldOverrides({
+          data: [dataFrame],
+          fieldConfig: {
+            overrides: [],
+            defaults: {},
+          }, replaceVariables: (value: string) => value,
+          theme
+        })[0])
+
+      })
+      .catch((e) => {
+        // catch errors and handle
+      });
+  }, []);
+
+  return (
+    <>
+      {data ? (
+        <div className="panel-container" style={{ width: 'auto' }}>
+          <Table {...props} data={data} />
+        </div>
+      ) : (
+          <LoadingPlaceholder />
+        )}
+    </>
   );
-};
+}
